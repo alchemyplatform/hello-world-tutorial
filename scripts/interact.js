07e6c4b7671fd51a89da912beba87bdc316fe03b
Albert Hu <albert@alchemy.com>
2021-10-16 00:43:00 -0700

    part 2

---
+++ scripts/interact.js
@@ -0,0 +1,28 @@
+const API_KEY = process.env.API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
+
+// provider - Alchemy
+const alchemyProvider = new ethers.providers.AlchemyProvider(network="ropsten", API_KEY);
+
+// signer - you
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+// contract instance
+const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+
+async function main() {
+    const message = await helloWorldContract.message();
+    console.log("The message is: " + message); 
+
+    console.log("Updating the message...");
+    const tx = await helloWorldContract.update("this is the new message");
+    await tx.wait();
+
+    const newMessage = await helloWorldContract.message();
+    console.log("The new message is: " + newMessage); 
+}
+
+main();
