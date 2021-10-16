212b0667b0a6b6e77b1659cbeaa667c2cbc3da3d
Albert Hu <albert@alchemy.com>
2021-10-16 00:47:36 -0700

    part 3

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,23 @@
-/**
-* @type import('hardhat/config').HardhatUserConfig
-*/
-
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 const { API_URL, PRIVATE_KEY } = process.env;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
-   solidity: "0.7.3",
-   defaultNetwork: "ropsten",
-   networks: {
+  solidity: "0.7.3",
+  defaultNetwork: "ropsten",
+  networks: {
       hardhat: {},
       ropsten: {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
       }
-   },
-}
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY
+  }
+};
